8c80648d20c137c830b0e464d62f28490c744b9a
marina.touceda <marina.touceda@gointegro.com>
2018-09-05 17:38:00 -0300

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemsListComponent } from './items-list/items-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemsListComponent
   ],
   imports: [
     BrowserModule
