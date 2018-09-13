90b7ada52aa62ae597d0dd5dd56a4d5cf897de26
marina.touceda <marina.touceda@gointegro.com>
2018-09-13 15:41:33 -0300

    Add some material components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-  myHero = this.heroes[0];
+export class AppComponent {  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,51 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { FormsModule} from '@angular/forms'
 import { AppComponent} from './app.component';
-import { ItemsComponent } from './items/items.component';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { InputComponent } from './components/input/input.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FirstPageComponent } from './components/first-page/first-page.component';
+import { SecondPageComponent } from './components/second-page/second-page.component';
+import { ThirdPageComponent } from './components/third-page/third-page.component';
+
+const appRoutes: Routes = [
+{path: 'first-page' ,component: FirstPageComponent},
+{path: 'second-page' ,component: SecondPageComponent},
+{path: 'third-page' ,component: ThirdPageComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ItemsComponent,   
+    AppComponent,   
     HeaderComponent,
     InputComponent,
     FooterComponent,   
+    NavComponent,
+    DashboardComponent,    
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent,   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
